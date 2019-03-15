3b64ee705d722739c2d937a7e68ddfe4a31758f0
raghavgai <raghavgaibond@gmail.com>
2019-03-15 06:00:05 +0530

    Stable my version of bokeh 201903150600

--- bokehjs/src/lib/models/layouts/layout_dom.ts
+++ bokehjs/src/lib/models/layouts/layout_dom.ts
@@ -35,8 +35,8 @@ export abstract class LayoutDOMView extends DOMView {
 
   layout: Layoutable
 
-  initialize(options: any): void {
-    super.initialize(options)
+  initialize(): void {
+    super.initialize()
     this.el.style.position = this.is_root ? "relative" : "absolute"
     this._child_views = {}
     this.build_child_views()
@@ -168,8 +168,7 @@ export abstract class LayoutDOMView extends DOMView {
   rebuild(): void {
     this.build_child_views()
     this.render()
-    this.root.update_layout()
-    this.root.compute_layout()
+    this.invalidate_layout()
   }
 
   compute_layout(): void {
